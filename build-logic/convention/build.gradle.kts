7ff8d0cfad9900ece6ef9cea4e06387de7e75c0b
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-20 19:33:26 +0700

    Create BuildKonfigConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,6 +11,8 @@ dependencies {
     compileOnly(libs.android.tools.common)
     compileOnly(libs.kotlin.gradlePlugin)
     compileOnly(libs.compose.gradlePlugin)
+    implementation(libs.buildkonfig.gradlePlugin)
+    implementation(libs.buildkonfig.compiler)
 }
 
 java {
@@ -56,6 +58,10 @@ gradlePlugin {
         register("cmpFeature") {
             id = "com.deeromptech.convention.cmp.feature"
             implementationClass = "CmpFeatureConventionPlugin"
+        }
+        register("buildKonfig") {
+            id = "com.deeromptech.convention.buildkonfig"
+            implementationClass = "BuildKonfigConventionPlugin"
         }
     }
 }
---
+++ build-logic/convention/src/main/kotlin/BuildKonfigConventionPlugin.kt
@@ -0,0 +1,30 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import com.codingfeline.buildkonfig.compiler.FieldSpec
+import com.codingfeline.buildkonfig.gradle.BuildKonfigExtension
+import com.deeromptech.chirp.convention.pathToPackageName
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+
+class BuildKonfigConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.codingfeline.buildkonfig")
+            }
+
+            extensions.configure<BuildKonfigExtension> {
+                packageName = target.pathToPackageName()
+                defaultConfigs {
+                    val apiKey = gradleLocalProperties(rootDir, rootProject.providers)
+                        .getProperty("API_KEY")
+                        ?: throw IllegalStateException(
+                            "Missing API_KEY property in local.properties"
+                        )
+                    buildConfigField(FieldSpec.Type.STRING, "API_KEY", apiKey)
+                }
+            }
+        }
+    }
+}
