ccc4959603924158fc5f5dbb323440fea19d7a92
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-20 19:08:03 +0700

    Create CmpLibraryConventionPlugin & CmpFeatureConventionPlugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -49,5 +49,13 @@ gradlePlugin {
             id = "com.deeromptech.convention.kmp.library"
             implementationClass = "KmpLibraryConventionPlugin"
         }
+        register("cmpLibrary") {
+            id = "com.deeromptech.convention.cmp.library"
+            implementationClass = "CmpLibraryConventionPlugin"
+        }
+        register("cmpFeature") {
+            id = "com.deeromptech.convention.cmp.feature"
+            implementationClass = "CmpFeatureConventionPlugin"
+        }
     }
 }
