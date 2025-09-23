c84096280c2ff0070e5c1fb2ad627c303e6b2333
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-23 08:12:35 +0700

    Implement ChirpAdaptiveFormLayout

---
+++ core/designsystem/src/commonMain/kotlin/com/deeromptech/core/designsystem/components/brand/ChirpBrandLogo.kt
@@ -0,0 +1,21 @@
+package com.deeromptech.core.designsystem.components.brand
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import chirp.core.designsystem.generated.resources.Res
+import chirp.core.designsystem.generated.resources.logo_chirp
+import org.jetbrains.compose.resources.vectorResource
+
+@Composable
+fun ChirpBrandLogo(
+    modifier: Modifier = Modifier
+) {
+    Icon(
+        imageVector = vectorResource(Res.drawable.logo_chirp),
+        contentDescription = null,
+        tint = MaterialTheme.colorScheme.primary,
+        modifier = modifier
+    )
+}
