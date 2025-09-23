a5f7857e2c06278d8b7fba8c0865088b85f73b60
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-23 08:21:24 +0700

    Implement ChirpSimpleSuccessLayout

---
+++ core/designsystem/src/commonMain/kotlin/com/deeromptech/core/designsystem/components/brand/ChirpSuccessIcon.kt
@@ -0,0 +1,22 @@
+package com.deeromptech.core.designsystem.components.brand
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import chirp.core.designsystem.generated.resources.Res
+import chirp.core.designsystem.generated.resources.success_checkmark
+import com.deeromptech.core.designsystem.theme.extended
+import org.jetbrains.compose.resources.vectorResource
+
+@Composable
+fun ChirpSuccessIcon(
+    modifier: Modifier = Modifier
+) {
+    Icon(
+        imageVector = vectorResource(Res.drawable.success_checkmark),
+        contentDescription = null,
+        tint = MaterialTheme.colorScheme.extended.success,
+        modifier = modifier
+    )
+}
