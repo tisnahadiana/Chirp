3d68c241a0a383f7a6c499ad475379a329a2c4d8
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-23 08:21:24 +0700

    Implement ChirpSimpleSuccessLayout

---
+++ core/designsystem/src/commonMain/kotlin/com/deeromptech/core/designsystem/components/layouts/ChirpSimpleSuccessLayout.kt
@@ -0,0 +1,100 @@
+package com.deeromptech.core.designsystem.components.layouts
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.deeromptech.core.designsystem.components.brand.ChirpSuccessIcon
+import com.deeromptech.core.designsystem.components.buttons.ChirpButton
+import com.deeromptech.core.designsystem.components.buttons.ChirpButtonStyle
+import com.deeromptech.core.designsystem.theme.ChirpTheme
+import com.deeromptech.core.designsystem.theme.extended
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ChirpSimpleSuccessLayout(
+    title: String,
+    description: String,
+    icon: @Composable () -> Unit,
+    primaryButton: @Composable () -> Unit,
+    secondaryButton: @Composable (() -> Unit)? = null,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        icon()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(y = -(25).dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.extended.textPrimary,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.extended.textSecondary,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(24.dp))
+
+            primaryButton()
+
+            if(secondaryButton != null) {
+                Spacer(modifier = Modifier.height(8.dp))
+                secondaryButton()
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ChirpSimpleSuccessLayoutPreview() {
+    ChirpTheme(darkTheme = true) {
+        ChirpSimpleSuccessLayout(
+            title = "Hello world!",
+            description = "Test description",
+            icon = {
+                ChirpSuccessIcon()
+            },
+            primaryButton = {
+                ChirpButton(
+                    text = "Log In",
+                    onClick = {},
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            },
+            secondaryButton = {
+                ChirpButton(
+                    text = "Resend verification email",
+                    onClick = {},
+                    style = ChirpButtonStyle.SECONDARY,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+            }
+        )
+    }
+}
