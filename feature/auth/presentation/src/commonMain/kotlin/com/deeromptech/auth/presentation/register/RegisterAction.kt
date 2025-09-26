87ffd5e0c87d89cf78b0810dec04f99a45a0d0f1
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-26 08:34:00 +0700

    Build register screen skeleton

---
+++ feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/register/RegisterAction.kt
@@ -0,0 +1,5 @@
+package com.deeromptech.auth.presentation.register
+
+sealed interface RegisterAction {
+
+}
---
+++ feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/register/RegisterState.kt
@@ -0,0 +1,6 @@
+package com.deeromptech.auth.presentation.register
+
+data class RegisterState(
+    val paramOne: String = "default",
+    val paramTwo: List<String> = emptyList(),
+)
