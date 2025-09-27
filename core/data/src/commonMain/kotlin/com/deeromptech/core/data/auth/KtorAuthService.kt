a8015b8f0370ad38d74138cdbed92462df55777b
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-27 14:16:47 +0700

    Implement register API endpoint

---
+++ core/data/src/commonMain/kotlin/com/deeromptech/core/data/auth/KtorAuthService.kt
@@ -0,0 +1,28 @@
+package com.deeromptech.core.data.auth
+
+import com.deeromptech.core.data.dto.requests.RegisterRequest
+import com.deeromptech.core.data.networking.post
+import com.deeromptech.core.domain.auth.AuthService
+import com.deeromptech.core.domain.util.DataError
+import com.deeromptech.core.domain.util.EmptyResult
+import io.ktor.client.HttpClient
+
+class KtorAuthService(
+    private val httpClient: HttpClient
+): AuthService {
+
+    override suspend fun register(
+        email: String,
+        username: String,
+        password: String
+    ): EmptyResult<DataError.Remote> {
+        return httpClient.post(
+            route = "/auth/register",
+            body = RegisterRequest(
+                email = email,
+                username = username,
+                password = password
+            )
+        )
+    }
+}
---
+++ core/data/src/commonMain/kotlin/com/deeromptech/core/data/dto/requests/RegisterRequest.kt
@@ -0,0 +1,10 @@
+package com.deeromptech.core.data.dto.requests
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RegisterRequest(
+    val email: String,
+    val username: String,
+    val password: String
+)
