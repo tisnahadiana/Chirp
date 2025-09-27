6b7156529d3394ddc66e44aefcc51b10228e0be7
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-27 14:16:47 +0700

    Implement register API endpoint

---
+++ core/domain/src/commonMain/kotlin/com/deeromptech/core/domain/auth/AuthService.kt
@@ -0,0 +1,12 @@
+package com.deeromptech.core.domain.auth
+
+import com.deeromptech.core.domain.util.DataError
+import com.deeromptech.core.domain.util.EmptyResult
+
+interface AuthService {
+    suspend fun register(
+        email: String,
+        username: String,
+        password: String
+    ): EmptyResult<DataError.Remote>
+}
