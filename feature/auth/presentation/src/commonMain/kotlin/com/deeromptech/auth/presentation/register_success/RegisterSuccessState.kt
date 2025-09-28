b33fc872adbb3ad86e2ea53e1229581b81c1e7cc
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-29 04:46:55 +0700

    Resend verification emails

--- feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/register_success/RegisterSuccessState.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/register_success/RegisterSuccessState.kt
@@ -1,6 +1,9 @@
 package com.deeromptech.auth.presentation.register_success
 
+import com.deeromptech.core.presentation.util.UiText
+
 data class RegisterSuccessState(
     val registeredEmail: String = "",
-    val isResendingVerificationEmail: Boolean = false
+    val isResendingVerificationEmail: Boolean = false,
+    val resendVerificationError: UiText? = null
 )
