901e18a6609d3e87dc8b13a344e19da86048514a
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-28 13:05:47 +0700

    Finish Koin setup & implement real-time validation of register fields

---
+++ feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/di/AuthPresentationModule.kt
@@ -0,0 +1,9 @@
+package com.deeromptech.auth.presentation.di
+
+import com.deeromptech.auth.presentation.register.RegisterViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val authPresentationModule = module {
+    viewModelOf(::RegisterViewModel)
+}
