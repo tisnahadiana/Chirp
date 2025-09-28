c5ebc7b48d7b6217dd19b2dabcb5c4b9264db98a
Tisna Hadiana <v00030159@danamon.co.id>
2025-09-29 05:18:25 +0700

    Email verification

--- feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/di/AuthPresentationModule.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/di/AuthPresentationModule.kt
@@ -1,5 +1,6 @@
 package com.deeromptech.auth.presentation.di
 
+import com.deeromptech.auth.presentation.email_verification.EmailVerificationViewModel
 import com.deeromptech.auth.presentation.register.RegisterViewModel
 import com.deeromptech.auth.presentation.register_success.RegisterSuccessViewModel
 import org.koin.core.module.dsl.viewModelOf
@@ -8,4 +9,5 @@ import org.koin.dsl.module
 val authPresentationModule = module {
     viewModelOf(::RegisterViewModel)
     viewModelOf(::RegisterSuccessViewModel)
+    viewModelOf(::EmailVerificationViewModel)
 }
--- feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/email_verification/EmailVerificationViewModel.kt
+++ feature/auth/presentation/src/commonMain/kotlin/com/deeromptech/auth/presentation/email_verification/EmailVerificationViewModel.kt
@@ -1,21 +1,32 @@
 package com.deeromptech.auth.presentation.email_verification
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.deeromptech.core.domain.auth.AuthService
+import com.deeromptech.core.domain.util.onFailure
+import com.deeromptech.core.domain.util.onSuccess
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class EmailVerificationViewModel : ViewModel() {
+class EmailVerificationViewModel(
+    private val authService: AuthService,
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
     private var hasLoadedInitialData = false
+
+    private val token = savedStateHandle.get<String>("token")
 
     private val _state = MutableStateFlow(EmailVerificationState())
     val state = _state
         .onStart {
             if (!hasLoadedInitialData) {
-                /** Load initial data here **/
+                verifyEmail()
                 hasLoadedInitialData = true
             }
         }
@@ -25,10 +36,29 @@ class EmailVerificationViewModel : ViewModel() {
             initialValue = EmailVerificationState()
         )
 
-    fun onAction(action: EmailVerificationAction) {
-        when (action) {
-            else -> TODO("Handle actions")
+    // NO-OP: Actions are purely for navigation
+    fun onAction(action: EmailVerificationAction) = Unit
+
+    private fun verifyEmail() {
+        viewModelScope.launch {
+            _state.update { it.copy(
+                isVerifying = true
+            ) }
+
+            authService
+                .verifyEmail(token ?: "Invalid token")
+                .onSuccess {
+                    _state.update { it.copy(
+                        isVerifying = false,
+                        isVerified = true
+                    ) }
+                }
+                .onFailure { _ ->
+                    _state.update { it.copy(
+                        isVerifying = false,
+                        isVerified = false
+                    ) }
+                }
         }
     }
-
 }
