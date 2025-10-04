d74951da2913aac4641bd9bc08f3c52762afadc5
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-04 14:19:32 +0700

    Implement token refresh

--- core/data/src/commonMain/kotlin/com/deeromptech/core/data/networking/HttpClientFactory.kt
+++ core/data/src/commonMain/kotlin/com/deeromptech/core/data/networking/HttpClientFactory.kt
@@ -1,10 +1,19 @@
 package com.deeromptech.core.data.networking
 
 import com.deeromptech.core.data.BuildKonfig
+import com.deeromptech.core.data.dto.AuthInfoSerializable
+import com.deeromptech.core.data.dto.requests.RefreshRequest
+import com.deeromptech.core.data.mappers.toDomain
+import com.deeromptech.core.domain.auth.SessionStorage
 import com.deeromptech.core.domain.logging.ChirpLogger
+import com.deeromptech.core.domain.util.onFailure
+import com.deeromptech.core.domain.util.onSuccess
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
@@ -12,13 +21,16 @@ import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.request.header
+import io.ktor.client.statement.request
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.serialization.json.Json
 
 class HttpClientFactory(
-    private val chirpLogger: ChirpLogger
+    private val chirpLogger: ChirpLogger,
+    private val sessionStorage: SessionStorage,
 ) {
 
     fun create(engine: HttpClientEngine): HttpClient {
@@ -48,6 +60,54 @@ class HttpClientFactory(
             defaultRequest {
                 header("x-api-key", BuildKonfig.API_KEY)
                 contentType(ContentType.Application.Json)
+            }
+
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        sessionStorage
+                            .observeAuthInfo()
+                            .firstOrNull()
+                            ?.let {
+                                BearerTokens(
+                                    accessToken = it.accessToken,
+                                    refreshToken = it.refreshToken
+                                )
+                            }
+                    }
+                    refreshTokens {
+                        if(response.request.url.encodedPath.contains("auth/")) {
+                            return@refreshTokens null
+                        }
+
+                        val authInfo = sessionStorage.observeAuthInfo().firstOrNull()
+                        if(authInfo?.refreshToken.isNullOrBlank()) {
+                            sessionStorage.set(null)
+                            return@refreshTokens null
+                        }
+
+                        var bearerTokens: BearerTokens? = null
+                        client.post<RefreshRequest, AuthInfoSerializable>(
+                            route = "/auth/refresh",
+                            body = RefreshRequest(
+                                refreshToken = authInfo.refreshToken
+                            ),
+                            builder = {
+                                markAsRefreshTokenRequest()
+                            }
+                        ).onSuccess { newAuthInfo ->
+                            sessionStorage.set(newAuthInfo.toDomain())
+                            bearerTokens = BearerTokens(
+                                accessToken = newAuthInfo.accessToken,
+                                refreshToken = newAuthInfo.refreshToken
+                            )
+                        }.onFailure { error ->
+                            sessionStorage.set(null)
+                        }
+
+                        bearerTokens
+                    }
+                }
             }
         }
     }
