e77bf13384df15671f6e03ea1740e4ff621bbc6b
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-10 14:53:24 +0700

    Implement adaptive dialog sheet layout

--- core/presentation/src/commonMain/kotlin/com/deeromptech/core/presentation/util/DeviceConfiguration.kt
+++ core/presentation/src/commonMain/kotlin/com/deeromptech/core/presentation/util/DeviceConfiguration.kt
@@ -21,6 +21,9 @@ enum class DeviceConfiguration {
     TABLET_LANDSCAPE,
     DESKTOP;
 
+    val isMobile: Boolean
+        get() = this in listOf(MOBILE_PORTRAIT, MOBILE_LANDSCAPE)
+
     companion object {
         fun fromWindowSizeClass(windowSizeClass: WindowSizeClass): DeviceConfiguration {
             return with(windowSizeClass) {
