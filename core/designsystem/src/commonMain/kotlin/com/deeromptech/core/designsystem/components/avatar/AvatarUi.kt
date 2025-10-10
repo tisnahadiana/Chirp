5662a2e823e18ed4dbcecc51dfb84208ab72ef50
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-10 14:00:20 +0700

    Implement stacked avatars layout

---
+++ core/designsystem/src/commonMain/kotlin/com/deeromptech/core/designsystem/components/avatar/AvatarUi.kt
@@ -0,0 +1,8 @@
+package com.deeromptech.core.designsystem.components.avatar
+
+data class AvatarUi(
+    val id: String,
+    val username: String,
+    val initials: String,
+    val imageUrl: String? = null
+)
