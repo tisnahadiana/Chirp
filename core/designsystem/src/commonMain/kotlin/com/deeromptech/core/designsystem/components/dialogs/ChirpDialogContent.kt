abed4893f1098a7247e1d320955ad2f3d466e422
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-10 14:50:32 +0700

    Implement dialog content wrapper

---
+++ core/designsystem/src/commonMain/kotlin/com/deeromptech/core/designsystem/components/dialogs/ChirpDialogContent.kt
@@ -0,0 +1,31 @@
+package com.deeromptech.core.designsystem.components.dialogs
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+
+@Composable
+fun ChirpDialogContent(
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Dialog(
+        onDismissRequest = onDismiss
+    ) {
+        Surface(
+            modifier = modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp)),
+            color = MaterialTheme.colorScheme.surface
+        ) {
+            content()
+        }
+    }
+}
