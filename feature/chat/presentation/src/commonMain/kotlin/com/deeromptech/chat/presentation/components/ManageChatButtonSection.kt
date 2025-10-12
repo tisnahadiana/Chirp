1c5f8486d61b6cb4e770f41a3a2de1347d8c3e66
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-12 09:08:06 +0700

    Small spacing fixes

--- feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/components/ManageChatButtonSection.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/components/ManageChatButtonSection.kt
@@ -2,6 +2,7 @@ package com.deeromptech.chat.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -14,7 +15,8 @@ fun ManageChatButtonSection(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier,
+        modifier = modifier
+            .padding(16.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.End)
     ) {
--- feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/create_chat/CreateChatScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/create_chat/CreateChatScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -74,6 +75,7 @@ fun CreateChatScreen(
             .wrapContentHeight()
             .imePadding()
             .background(MaterialTheme.colorScheme.surface)
+            .navigationBarsPadding()
     ) {
         AnimatedVisibility(
             visible = !shouldHideHeader
