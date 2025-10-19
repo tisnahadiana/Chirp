68b601ff99b5cbc38170759cb283fc61cd70059f
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-19 09:27:30 +0700

    Implement chat list item UI

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/mappers/ChatMappers.kt
@@ -0,0 +1,17 @@
+package com.deeromptech.chat.presentation.mappers
+
+import com.deeromptech.chat.domain.models.Chat
+import com.deeromptech.chat.presentation.model.ChatUi
+
+fun Chat.toUi(localParticipantId: String): ChatUi {
+    val (local, other) = participants.partition { it.userId == localParticipantId }
+    return ChatUi(
+        id = id,
+        localParticipant = local.first().toUi(),
+        otherParticipants = other.map { it.toUi() },
+        lastMessage = lastMessage,
+        lastMessageSenderUsername = participants
+            .find { it.userId == lastMessage?.senderId }
+            ?.username
+    )
+}
