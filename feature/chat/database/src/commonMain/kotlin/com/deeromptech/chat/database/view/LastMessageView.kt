ff3154888734435dd34d6d5f3a04401f84c3de24
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-25 15:01:33 +0700

    Implement last message view

---
+++ feature/chat/database/src/commonMain/kotlin/com/deeromptech/chat/database/view/LastMessageView.kt
@@ -0,0 +1,23 @@
+package com.deeromptech.chat.database.view
+
+import androidx.room.DatabaseView
+
+@DatabaseView(
+    viewName = "last_message_view_per_chat",
+    value = """
+        SELECT m1.*
+        FROM chatmessageentity m1
+        JOIN (
+            SELECT chatId, MAX(timestamp) AS max_timestamp
+            FROM chatmessageentity
+            GROUP BY chatId
+        ) m2 ON m1.chatId = m2.chatId AND m1.timestamp = m2.max_timestamp
+    """
+)
+data class LastMessageView(
+    val messageId: String,
+    val chatId: String,
+    val senderId: String,
+    val content: String,
+    val timestamp: Long
+)
