216ea9677215ec88654948df5772285ae253e898
Tisna Hadiana <v00030159@danamon.co.id>
2025-10-26 11:22:21 +0700

    Move message list out of chat header

---
+++ feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/components/EmptySection.kt
@@ -0,0 +1,57 @@
+package com.deeromptech.chat.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import chirp.feature.chat.presentation.generated.resources.Res
+import chirp.feature.chat.presentation.generated.resources.empty_chat
+import com.deeromptech.core.designsystem.theme.extended
+import com.deeromptech.core.presentation.util.DeviceConfiguration
+import com.deeromptech.core.presentation.util.currentDeviceConfiguration
+import org.jetbrains.compose.resources.painterResource
+
+@Composable
+fun EmptySection(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier
+) {
+    val configuration = currentDeviceConfiguration()
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.empty_chat),
+            contentDescription = title,
+            modifier = Modifier.size(
+                if(configuration == DeviceConfiguration.MOBILE_LANDSCAPE) {
+                    125.dp
+                } else {
+                    200.dp
+                }
+            )
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = title,
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.extended.textPrimary
+        )
+        Text(
+            text = description,
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.extended.textSecondary
+        )
+    }
+}
