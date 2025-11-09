a4ca27244cfc61697e6e2c1ff93297b8c3c9b22b
Tisna Hadiana <v00030159@danamon.co.id>
2025-11-09 09:08:52 +0700

    Group messages by date

--- feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/util/DateUtils.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/util/DateUtils.kt
@@ -2,9 +2,12 @@ package com.deeromptech.chat.presentation.util
 
 import chirp.feature.chat.presentation.generated.resources.Res
 import chirp.feature.chat.presentation.generated.resources.today
+import chirp.feature.chat.presentation.generated.resources.today_x
 import chirp.feature.chat.presentation.generated.resources.yesterday
+import chirp.feature.chat.presentation.generated.resources.yesterday_x
 import com.deeromptech.core.presentation.util.UiText
 import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.format
@@ -42,9 +45,32 @@ object DateUtils {
         )
 
         return when(messageDateTime.date) {
-            todayDate -> UiText.Resource(Res.string.today, arrayOf(formattedTime))
-            yesterdayDate -> UiText.Resource(Res.string.yesterday, arrayOf(formattedTime))
+            todayDate -> UiText.Resource(Res.string.today_x, arrayOf(formattedTime))
+            yesterdayDate -> UiText.Resource(Res.string.yesterday_x, arrayOf(formattedTime))
             else -> UiText.DynamicString(formattedDateTime)
+        }
+    }
+
+    fun formatDateSeparator(date: LocalDate, clock: Clock = Clock.System): UiText {
+        val timeZone = TimeZone.currentSystemDefault()
+        val today = clock.now().toLocalDateTime(timeZone).date
+        val yesterday = today.minus(1, DateTimeUnit.DAY)
+
+        return when(date) {
+            today -> UiText.Resource(Res.string.today)
+            yesterday -> UiText.Resource(Res.string.yesterday)
+            else -> {
+                val formatted = date.format(
+                    LocalDate.Format {
+                        day()
+                        char('/')
+                        monthNumber()
+                        char('/')
+                        year()
+                    }
+                )
+                UiText.DynamicString(formatted)
+            }
         }
     }
 }
