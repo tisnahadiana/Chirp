448bbcefb3089a12269d98c95f2e436b203ce364
Tisna Hadiana <v00030159@danamon.co.id>
2025-11-25 14:41:46 +0700

    Give message field proper IME padding and request focus when tapping inside

--- feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/chat_detail/ChatDetailScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/chat_detail/ChatDetailScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -295,6 +296,7 @@ fun ChatDetailScreen(
                                 },
                                 modifier = Modifier
                                     .fillMaxWidth()
+                                    .imePadding()
                                     .padding(
                                         vertical = 8.dp,
                                         horizontal = 16.dp
@@ -323,6 +325,7 @@ fun ChatDetailScreen(
                             },
                             modifier = Modifier
                                 .fillMaxWidth()
+                                .imePadding()
                                 .padding(8.dp)
                         )
                     }
