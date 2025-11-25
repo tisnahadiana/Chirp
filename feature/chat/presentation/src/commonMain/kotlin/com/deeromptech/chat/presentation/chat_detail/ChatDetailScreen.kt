245a5b9fc1496dd24d560ad20a5abc0c97b04b50
Tisna Hadiana <v00030159@danamon.co.id>
2025-11-25 20:11:37 +0700

    Fix visual bug that could cause messages to overlap on tablet devices when switching between chats

--- feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/chat_detail/ChatDetailScreen.kt
+++ feature/chat/presentation/src/commonMain/kotlin/com/deeromptech/chat/presentation/chat_detail/ChatDetailScreen.kt
@@ -107,6 +107,12 @@ fun ChatDetailRoot(
         viewModel.onAction(ChatDetailAction.OnSelectChat(chatId))
     }
 
+    LaunchedEffect(chatId, state.messages) {
+        if(state.messages.isNotEmpty()) {
+            messageListState.scrollToItem(0)
+        }
+    }
+
     BackHandler(
         enabled = !isDetailPresent
     ) {
